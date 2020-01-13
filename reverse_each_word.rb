612c992667bb54a9fb7702efaf8e3732b5305d98
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-13 22:02:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
 def reverse_each_word(sentence)
  array=sentence.split(' ')
  newArr=[]
- newArr=array.collect(|word| word.reverse)
+ newArr=array.collect{|word| word.reverse}
  newArr.join(' ')
 end
+
+# def reverse_each_word(sentence)
+#   words_array = sentence.split(" ")
+#   new_array = []
+#   new_array = words_array.collect {|word| word.reverse }
+#   new_array.join(" ")
+# end
